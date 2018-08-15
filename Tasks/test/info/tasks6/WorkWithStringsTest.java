eb177ec23a06e6ab0c239136a7a139279a1a1c98
Copoka Alex <copokaalekc@gmai.com>
2018-08-15 20:21:24 +0300

    Add work with cycle and test

---
+++ Tasks/test/info/tasks6/WorkWithStringsTest.java
@@ -0,0 +1,21 @@
+package info.tasks6;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WorkWithStringsTest {
+
+	@Test
+	public void testMult() {
+		assertNotNull(WorkWithStrings.mult());
+	}
+
+	@Test
+	public void testSpace() {
+		assertNotNull(WorkWithStrings.space());
+		
+
+	}
+
+}
